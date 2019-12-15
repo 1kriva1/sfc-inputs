d122c78140a2664ff58adf02f17ab2eeb09d7d4a
Andrii Krivoruk <krivorukandrey@gmail.com>
2019-12-15 21:17:13 +0200

    final result of mask

--- projects/sfc-inputs/src/lib/sfc-inputs.module.ts
+++ projects/sfc-inputs/src/lib/sfc-inputs.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextInputComponent } from './sfc-text-input/sfc-text-input.component';
 import { InputRefDirective } from './common/input-ref.directive';
+import { SfcMaskDirective } from './sfc-mask/sfc-mask.directive';
 
 
 @NgModule({
-  declarations: [TextInputComponent, InputRefDirective],
+  declarations: [TextInputComponent, InputRefDirective, SfcMaskDirective],
   imports: [
     CommonModule
   ],
-  exports: [TextInputComponent, InputRefDirective]
+  exports: [TextInputComponent, InputRefDirective, SfcMaskDirective]
 })
 export class SfcInputsModule { }
--- projects/sfc-inputs/src/public-api.ts
+++ projects/sfc-inputs/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of sfc-inputs
  */
 
+export * from './lib/sfc-mask/sfc-mask.directive';
 export * from './lib/common/input-ref.directive';
 export * from './lib/sfc-text-input/sfc-text-input.component';
 export * from './lib/sfc-inputs.module';
