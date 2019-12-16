54db74cb16150f56d81645bc88060e8d3a280159
Andrii Kryvoruk <AndriiK@neogames.com>
2019-12-16 12:22:50 +0200

    initial

--- projects/sfc-inputs/src/public-api.ts
+++ projects/sfc-inputs/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of sfc-inputs
  */
 
-export * from './lib/sfc-mask/sfc-mask.directive';
-export * from './lib/common/input-ref.directive';
+
 export * from './lib/sfc-text-input/sfc-text-input.component';
 export * from './lib/sfc-inputs.module';
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,14 +3,12 @@ import { AppComponent } from "./app.component";
 import { CommonModule } from '@angular/common';
 import { TextInputComponent } from "../../projects/sfc-inputs/src/lib/sfc-text-input/sfc-text-input.component";
 import { DebugElement } from "@angular/core";
-import { InputRefDirective } from "../../projects/sfc-inputs/src/lib/common/input-ref.directive";
 import {By} from "@angular/platform-browser";
 
 describe("Component: AppComponent", () => {
     let fixture: ComponentFixture<AppComponent>;
     let component: AppComponent;
     let el: DebugElement;
-    let emailField: DebugElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -19,7 +17,6 @@ describe("Component: AppComponent", () => {
             fixture = TestBed.createComponent(AppComponent);
             el = fixture.debugElement;
             component = el.componentInstance;
-            emailField = el.query(By.css('#email-input'));
 
             fixture.detectChanges();
         });
@@ -28,17 +25,4 @@ describe("Component: AppComponent", () => {
     it("should be defined ", async(() => {
         expect(component).toBeTruthy();
     }));
-
-    it("should create a sfc text input", async(() => {
-        expect(emailField).toBeTruthy();
-    }))
-
-    it("should create an icon inside sfc text input", async(() => {
-        //console.log(emailField.nativeElement);
-        expect(emailField.query(By.css('i.icon.fa.fa-envelope'))).toBeTruthy();
-    }))
-
-    it("should create an input text in sfc text input", async(() => {
-        expect(emailField.query(By.css('input.test-class'))).toBeTruthy();
-    }))
 });
