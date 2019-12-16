9eede4a87941c79cedb64cc9aa4f1abfaa8635d4
Andrii Kryvoruk <AndriiK@neogames.com>
2019-12-16 11:59:27 +0200

    after cource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { TextInputComponent } from '../../projects/sfc-inputs/src/lib/sfc-text-input/sfc-text-input.component';
-import {SfcInputsModule} from '../../projects/sfc-inputs/src/lib/sfc-inputs.module'
+import {SfcInputsModule} from '../../projects/sfc-inputs/src/lib/sfc-inputs.module';
+
 
 @NgModule({
     imports: [BrowserModule, FormsModule, SfcInputsModule],
